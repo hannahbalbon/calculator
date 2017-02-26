94690149c05735c2ca8753b63c618dd0b27be91a
Hannah Balbon <hannah.balbon@yahoo.com>
2017-02-26 15:56:51 -0800

    initial js

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,26 @@
+// Front-end logic
 $(document).ready(function() {
   $("#blanks form").submit(function(event) {
-    var blanks = ["name", "hello", "would-you-rather", "adventurous", "noun"];
-
-    blanks.forEach(function(blank) {
-      var userInput = $("input." + blank).val();
-      $("." + blank).text(userInput).val();
-    });
-
-    $("#story").show();
-
     event.preventDefault();
+    var US = $("select#hello").val();
+    var Latin America = $("select#hola").val();
+    var Italy = $("select#ciao").val();
+    var China = $("select#nihao").val();
+    var Japan = $("select#konnichiwa").val();
+    var Philippines = $("#kamusta").val();
+
+    var vacation = 
+
+    if (hello === hello)  {
+      $("hello").show(explore the U.S.);
+    } else if (hello === hola)  {
+      $("#hello").show("You are ready to branch out in Latin America");
+    } else if (hello === ciao)  {
+      $("#hello").show();
+
+
+    $("#vacation").show();
+
+
   });
 });
