e22c3bfa0cd9ea2662d779fc5b62fd4384c73ba0
Hannah Balbon <hannah.balbon@yahoo.com>
2017-02-27 20:29:54 -0800

    Add README.md file

---
+++ README.md
@@ -0,0 +1,32 @@
+# _Vacation Suggester_
+
+#### _HTML, CSS, Bootstrap, jQuery, and Javascript Exercise for Epicodus 02.26.2017_
+
+#### By _**Hannah Balbon**_
+
+## Description
+
+_This webpage is an exercise in basic HTML, CSS, Bootstrap, jQuery, and Javascript for Epicodus - Intro to Programming Evening Class. The form gathers input from the user, uses branching to return destination, utilizes jQuery to show and hide destination, and uses bootstrap and custom CSS to style the webpage._
+
+## Setup/Installation Requirements
+
+* _Clone this repository_
+* _Open in a simple HTTP server in the top level of the cloned directory_
+* _Then open your web browser of choice_
+
+
+## Support and contact details
+
+_If you run into any issues or have questions, ideas or concerns.  Please contact Hannah Balbon at hannah.balbon@yahoo.com}_
+
+## Technologies Used
+
+* HTML
+* CSS
+* Bootstrap
+* jQuery
+* Javascript
+
+### License
+
+Copyright (c) 2017 **_Hannah Balbon_**
--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,31 @@
-// Front-end logic
 $(document).ready(function() {
-  $("#blanks form").submit(function(event) {
+  $("form#blanks").submit(function(event) {
     event.preventDefault();
     var US = $("select#hello").val();
     var Latin America = $("select#hola").val();
     var Italy = $("select#ciao").val();
     var China = $("select#nihao").val();
     var Japan = $("select#konnichiwa").val();
-    var Philippines = $("#kamusta").val();
+    var Philippines = $("select#kamusta").val();
+    var final-vacation = $("#final-vacation").val();
 
-    var vacation = 
-
+    var final-vacation;
     if (hello === hello)  {
-      $("hello").show(explore the U.S.);
+      $("how-hello").show(explore the U.S.);
     } else if (hello === hola)  {
-      $("#hello").show("You are ready to branch out in Latin America");
+      $("#how-hello").show("You are ready to branch out in Latin America");
     } else if (hello === ciao)  {
-      $("#hello").show();
+      $("#how-hello").show("Nel Blu di pinto di blu in Italy!");
+    } else if (hello === nihao)  {
+      $("#how-hello").show("The great wall of CHINA is calling you!");
+    } else if (hello === konnichiwa)  {
+      $("#how-hello").show("Try sushi, ramen, and play with Super Mario in Japan!");
+    } else (hello === kamusta)  {
+      $("#how-hello").show("Find out why heaven is a place on earth in the Philippines!");
+    }
 
-
-    $("#vacation").show();
-
+    $("#how-hello").append(final-vacation);
+    $("#final-vacation").show();
 
   });
 });
